d5e330809695c0b905e8b41e5ff74ef17c01a2fd
Danny <gabriel.danjon@epitech.eu>
2021-11-24 19:39:40 +0900

    [ADD]: Coliders / movements

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    Vector3 _cameraOffset;
+    [Range(0.01f, 1f)]
+    public float smoothFactor = 0.5f;
+    public bool lookAtPlayer = false;
+    public bool rotateAroundPlayer = true;
+    public float rotationSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rotateAroundPlayer && Input.GetMouseButton(0)) {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+            _cameraOffset = camTurnAngle * _cameraOffset;
+        }
+
+        Vector3 newPosition = playerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
+        if (lookAtPlayer || rotateAroundPlayer)
+            transform.LookAt(playerTransform);
+        transform.eulerAngles = transform.eulerAngles - new Vector3(10f, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    CharacterController _characterController;
+    Animator _animator;
+    public float StartAnimTime = 0.3f;
+    [Range(0, 1f)]
+    public float StopAnimTime = 1f;
+    public float allowPlayerRotation = 0.1f;
+    public float desiredRotationSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        MoveCharacter();
+    }
+
+    void MoveCharacter() {
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        float zMovement = Input.GetAxisRaw("Vertical");
+        bool isMoving = (xMovement != 0) || (zMovement > 0);
+
+
+        if (zMovement < 0)
+            zMovement = 0;
+
+        transform.Rotate(0f, xMovement * 3.5f, 0f);
+
+        Vector3 moveDir = Vector3.forward * zMovement;
+
+        moveDir = transform.TransformDirection(moveDir);
+        moveDir *= movementSpeed;
+
+        _characterController.Move(moveDir);
+
+        _animator.SetBool("Run", isMoving);
+    }
+}
