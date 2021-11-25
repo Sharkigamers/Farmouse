4200ec5126973ff809a7681994155249bd31efbb
Danny <gabriel.danjon@epitech.eu>
2021-11-25 15:44:23 +0900

    [ADD]: Animals following player when entering in the area

---
+++ Assets/AnimalMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Transform target;
+    Animator anim;
+
+    bool canMove = false;
+    bool canFollow = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (canFollow)
+            FollowTargetWithRotation(target, 3f, 5f, 20f);
+    }
+
+    void FollowTargetWithRotation(Transform target, float distanceToStop,float distanceToStart, float speed)
+     {
+         transform.LookAt(target);
+
+         if (Vector3.Distance(transform.position, target.position) <= distanceToStop) {
+            anim.SetInteger("Walk", 0);
+            canMove = false;
+         }
+         else if((!canMove && Vector3.Distance(transform.position, target.position) > distanceToStart) || canMove)
+         {
+             canMove = true;
+             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.08f);
+             anim.SetInteger("Walk", 1);
+         } else {
+            anim.SetInteger("Walk", 0);
+            canMove = false; 
+         }
+     }
+
+     private void OnTriggerEnter(Collider other) {
+         if (!canFollow)
+             if (other.CompareTag("Player"))
+                canFollow = true;
+     }
+}
